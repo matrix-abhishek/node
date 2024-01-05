f71091894990e237397c95917393d2b326a4f485
Abhishek Pal <abhishek.pal@devs.matrixmarketers.com>
2024-01-05 17:47:37 +0530

    Add model & views

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -14,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/',shopRoutes);
 
 app.use(errorController.get404);
 
